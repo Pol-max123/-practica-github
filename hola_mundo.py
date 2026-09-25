4be87a649db16836e4cc8fa086b818fa217b28d4
Pol-max123 <pol.romeu@campus.xaloc.org>
2026-09-25 09:51:29 +0200

    He añadido código

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+variable1=int(input("introducevalor1")) 
+variable2=int(input("introducevalor2")) 
+
+if variable1>variable2:
+    print("variable1 es mayor que variable2")
+else:
+    print("variable es menor ")
+    
